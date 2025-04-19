664e13bf6b1226cf778b2e391c9ef6463dd27aa3
Mohamed Shaad <98437584+shaadclt@users.noreply.github.com>
2025-04-19 15:37:25 +0530

    LICENSE

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="eval-rag",
-    version="0.0.1",
+    version="0.0.2",
     description="A comprehensive evaluation toolkit for assessing Retrieval-Augmented Generation (RAG) outputs using linguistic, semantic, and fairness metrics",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author="Mohamed Shaad",
     author_email="shaadclt@gmail.com",
-    url="https://github.com/shaadclt/Eval-RAG",
+    url="https://github.com/shaadclt/EvalRAG",
     packages=find_packages(),
     install_requires=[
         "torch",
